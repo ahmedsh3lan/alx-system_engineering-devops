1ad97288d82915f885b73772facd661588d3f0fe
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2024-03-23 04:55:18 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,3 +4,7 @@ package { 'flask':
   ensure   => '2.1.0',  # Ensure Flask is installed with version 2.1.0
   provider => 'pip3',   # Use pip3 as the provider
 }
+package { 'werkzug':
+  ensure   => '2.1.0',
+  provider => 'pip3',   
+}
