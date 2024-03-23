6f9be370c818bd1ffb792d4a3b43b142c5a39336
Ahmed Awad <67976749+ahmedsh3lan@users.noreply.github.com>
2024-03-23 04:52:32 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # Install flask
 package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+  ensure   => '2.1.0',  # Ensure Flask is installed with version 2.1.0
+  provider => 'pip3',   # Use pip3 as the provider
 }
